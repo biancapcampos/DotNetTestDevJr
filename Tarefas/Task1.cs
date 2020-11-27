799b58ec5ab552454ce5eddae708199c3961eccc
biancapcampos <75132704+biancapcampos@users.noreply.github.com>
2020-11-27 13:23:03 -0300

    Update and rename Tasks/Task1.cs to Tarefas/Task1.cs

---
+++ Tarefas/Task1.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Tasks
+{
+    public class Task1
+    {
+        /*
+         * Dada a lista de inteiros, retorne o maior número da lista         
+         */
+[TestMethod] 
+        public void Test1(
+        {
+            var result = Task1.GetMax(new List<int> { 1, 4, 3, 7, 12 });
+
+            if (result != 12)
+                Assert.Fail();
+        }
+            public static int? GetMax(List<int> list) { }
+    }
+}
